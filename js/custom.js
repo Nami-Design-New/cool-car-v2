891ab0b9a5771f6f0d34972578928b1d05e75ce3
AbdelfattahEmad <abdelfattahemad1299@gmail.com>
2024-08-21 14:02:48 +0300

    img fix

--- js/custom.js
+++ js/custom.js
@@ -71,8 +71,8 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
   document.body.classList.toggle("light-theme");
   if (document.body.classList.contains("light-theme")) {
-    icon.src = "/img/icons/darks.svg";
+    icon.src = "img/icons/darks.svg";
   } else {
-    icon.src = "/img/icons/sun.svg";
+    icon.src = "img/icons/sun.svg";
   }
 };
